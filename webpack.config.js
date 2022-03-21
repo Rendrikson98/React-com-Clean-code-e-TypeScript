b6065fe5e3bb22db8828f19f12503e284d116f80
Rendrikson Oliveira <rendrikson16@gmail.com>
2022-03-20 23:56:21 -0300

    chore: config sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,48 @@
-const path = require('path');
+const path = require('path')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
   mode: 'development',
   entry: './src/main/index.tsx',
   output: {
-    path: path.join(__dirname, "public/js"),
+    path: path.join(__dirname, 'public/js'),
     publicPath: '/public/js',
     filename: 'bundle.js'
   },
   resolve: {
-    extensions: ['.ts', '.tsx', '.js'],
+    extensions: ['.ts', '.tsx', '.js', 'scss'],
     alias: {
       '@': path.join(__dirname, 'src')
     }
   },
+  module: {
+    rules: [{
+      test: /\.ts(x?)$/,
+      loader: 'ts-loader',
+      exclude: /node_modules/
+    },
+    {
+      test: /\.scss$/,
+      use: [{
+        loader: 'style-loader'
+      }, {
+        loader: 'css-loader',
+        option: {
+          module: true
+        }
+      }, {
+        loader: 'sass-loader'
+      }]
+    }]
+  },
   devServer: {
-    contentBase: "./public",
+    contentBase: './public',
     writeToDisk: true,
     historyApiFallBack: true
   },
   extenal: {
-    react: "React",
-    "react-dom": "ReactDOM"
+    react: 'React',
+    'react-dom': 'ReactDOM'
   },
   plugin: [
     new CleanWebpackPlugin()
