31a2c41a28ffe398f44e440feed47b8cd9943124
Rendrikson98 <rendrikson16@gmail.com>
2022-06-10 16:44:43 -0300

    feat: ensure Login calls validation with correct email

--- src/presentation/componentes/input/input.tsx
+++ src/presentation/componentes/input/input.tsx
@@ -5,8 +5,17 @@ import Context from '@/presentation/contexts/form/form-context'
 type Props = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
 
 const Input = (props: Props) => {
-  const {errorState} = useContext(Context);
-  const error = errorState[props.name]
+  const {state, setState} = useContext(Context);
+  const error = state[`${props.name}Error`]
+  const enableInput = (event: React.FocusEvent<HTMLInputElement>): void => {
+    event.target.readOnly = false
+  }
+  const handleChange = (event: React.FocusEvent<HTMLInputElement>): void => {
+    setState({
+      ...state,
+      [event.target.name]: event.target.value
+    });
+  }
   const getStatus = (): string => {
     return '🔴'
   }
@@ -15,8 +24,8 @@ const Input = (props: Props) => {
   }
   return (
     <div className={Styles.inputWrap}>
-      <input {...props} />
-      <span data-testid={`${props.name}`} title={getTitle()} className={Styles.status}>{getStatus()}</span>
+      <input {...props} data-testid={props.name} readOnly onFocus={enableInput} onChange={handleChange}/>
+      <span data-testid={`${props.name}-status`} title={getTitle()} className={Styles.status}>{getStatus()}</span>
     </div>
   )
 }
--- src/presentation/componentes/router/router.tsx
+++ src/presentation/componentes/router/router.tsx
@@ -1,11 +1,21 @@
 import Login from '@/presentation/pages/login/login'
+import { Validation } from '@/presentation/protocols/validation'
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+class ValidationSpy implements Validation {
+  errorMessage:string
+  input:object
+  validate(input: object): string {
+      this.input = input
+      return this.errorMessage
+  }
+}
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/login' element={<Login />} />
+        <Route path='/login' element={<Login validation={new ValidationSpy()} />} />
       </Routes>
     </BrowserRouter>
   )
