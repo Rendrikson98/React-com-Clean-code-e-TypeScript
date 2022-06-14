7107267743acfcca04134ecab0946f972605f94a
Rendrikson98 <rendrikson16@gmail.com>
2022-06-14 12:55:07 -0300

    feat: ensure Login disable submit button only if form is invalid

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -57,6 +57,16 @@ describe('Login component', ()=>{
         expect(passwordStatus.textContent).toBe('🔴');
     })
 
+    test('Should show valid email state if Validation succeeds', () => {
+        const {sut, validationStub} = makeSut();
+        validationStub.errorMessage = null;
+        const emailInput = sut.getByTestId('email');
+        fireEvent.input(emailInput, {target: {value: faker.internet.email()}});
+        const emailStatus = sut.getByTestId('email-status');
+        expect(emailStatus.title).toBe("Tudo certo!");
+        expect(emailStatus.textContent).toBe('🟢');
+    })
+
     test('Should show valid password state if Validation succeeds', () => {
         const {sut, validationStub} = makeSut();
         validationStub.errorMessage = null;
@@ -66,4 +76,16 @@ describe('Login component', ()=>{
         expect(passwordStatus.title).toBe("Tudo certo!");
         expect(passwordStatus.textContent).toBe('🟢');
     })
+
+    test('Should enable submit button if form is valid', () => {
+        const {sut, validationStub} = makeSut();
+        validationStub.errorMessage = null;
+        const emailInput = sut.getByTestId('email');
+        fireEvent.input(emailInput, {target: {value: faker.internet.email()}});
+        const passwordInput = sut.getByTestId('password');
+        fireEvent.input(passwordInput, {target: {value: faker.internet.password()}}); 
+        const submitButton = sut.getByTestId('submit') as HTMLButtonElement
+        expect(submitButton.disabled).toBe(false)
+    })
+
 })
