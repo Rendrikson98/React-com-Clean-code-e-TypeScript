0d8417601f230612bda653d9a9f50cd604b18f58
Rendrikson98 <rendrikson16@gmail.com>
2022-06-26 21:54:12 -0300

    refactor: move SUT creation to a factorey method

--- src/validation/required-field/required-field-validation.spec.ts
+++ src/validation/required-field/required-field-validation.spec.ts
@@ -1,15 +1,17 @@
 import { RequiredFieldError } from "../errors";
 import { RequiredFieldValidation } from "./required-field-validation";
-import faker from 'faker'
+import faker from 'faker';
+
+const makesut = (): RequiredFieldValidation => new RequiredFieldValidation(faker.database.column())
 
 describe('RequireFieldValidation', ()=>{
     test('Should return error if field is empty', ()=> {
-        const sut = new RequiredFieldValidation('email');
+        const sut = makesut();
         const error = sut.validate('');
         expect(error).toEqual(new RequiredFieldError());
     })
     test('Should return error if field is not empty', ()=> {
-        const sut = new RequiredFieldValidation('email');
+        const sut = makesut();
         const error = sut.validate(faker.random.word());
         expect(error).toBeFalsy()
     })
