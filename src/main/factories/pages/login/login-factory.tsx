1692b04a939e008be84b18ed9c8ee89694f160b1
Rendrikson98 <rendrikson16@gmail.com>
2022-07-26 12:01:19 -0300

    feat: ensure Login calls SAveAccessToken with correct value

--- src/main/factories/pages/login/login-factory.tsx
+++ src/main/factories/pages/login/login-factory.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Login from '@/presentation/pages/login/login';
 import { MakeRemoteAuthentication } from '../../usecases/authentication/remote-authentication-factory';
 import { MakeLoginValidation } from './login-validation-factory';
+import { SaveAccessTokenMock } from '@/presentation/test/mock-save-access-token';
 
 const MakeLogin: React.FC = () => {
   return (
     <Login
       authentication={MakeRemoteAuthentication()}
       validation={MakeLoginValidation()}
+      saveAccessToken={new SaveAccessTokenMock()} //mudar pra o saveAccssToken de verdade
     />
   );
 };
--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,17 +1,22 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 import { Footer, Form, Input, LoginHeader } from '../../componentes';
 import Styles from './login-styles.scss';
-import Context from "@/presentation/contexts/form/form-context"
-import { Validation } from "@/presentation/protocols/validation";
-import { Authentication } from "@/domain/usecases";
-import { Link, useNavigate } from "react-router-dom";
+import Context from '@/presentation/contexts/form/form-context';
+import { Validation } from '@/presentation/protocols/validation';
+import { Authentication, SaveAccessToken } from '@/domain/usecases';
+import { Link, useNavigate } from 'react-router-dom';
 
 type Props = {
-  validation: Validation,
-  authentication: Authentication
-}
+  validation: Validation;
+  authentication: Authentication;
+  saveAccessToken: SaveAccessToken;
+};
 
-const Login: React.FC<Props> = ({validation, authentication}: Props) => {
+const Login: React.FC<Props> = ({
+  validation,
+  authentication,
+  saveAccessToken,
+}: Props) => {
   const history = useNavigate();
   const [state, setState] = useState({
     isLoading: false,
@@ -19,54 +24,71 @@ const Login: React.FC<Props> = ({validation, authentication}: Props) => {
     password: '',
     emailError: '',
     passwordError: '',
-    mainError: ''
-    
+    mainError: '',
   });
 
-  useEffect(()=>{
+  useEffect(() => {
     setState({
       ...state,
       emailError: validation.validate('email', state.email),
-      passwordError: validation.validate('password', state.password)
-    })
-  }, [state.email, state.password])
+      passwordError: validation.validate('password', state.password),
+    });
+  }, [state.email, state.password]);
 
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>): Promise<void> => {
+  const handleSubmit = async (
+    event: React.FormEvent<HTMLFormElement>
+  ): Promise<void> => {
     event.preventDefault();
     try {
-      if(state.isLoading || state.emailError || state.passwordError){
-        return
+      if (state.isLoading || state.emailError || state.passwordError) {
+        return;
       }
-      setState({...state, isLoading: true})
+      setState({ ...state, isLoading: true });
       const account = await authentication.auth({
         email: state.email,
-        password: state.password
-      })
-      localStorage.setItem('accessToken', account.accessToken);
-      history('/')
+        password: state.password,
+      });
+      await saveAccessToken.save(account.accessToken);
+      history('/');
     } catch (error) {
-      setState({...state, isLoading: false, mainError: error.message})
-      console.log(state.mainError)
+      setState({ ...state, isLoading: false, mainError: error.message });
+      console.log(state.mainError);
     }
-    
-  }
+  };
 
   return (
     <div className={Styles.login}>
       <LoginHeader />
-      <Context.Provider value={{state, setState}}>
-        <form data-testid="form" className={Styles.form} onSubmit={handleSubmit}>
+      <Context.Provider value={{ state, setState }}>
+        <form
+          data-testid="form"
+          className={Styles.form}
+          onSubmit={handleSubmit}
+        >
           <h2>Login</h2>
-          < Input type='email' name='email' placeholder='Digite seu e-mail' />
-          < Input type='password' name='password' placeholder='Digite sua senha' />
-          <button data-testid="submit" disabled={!!state.emailError || !!state.passwordError} className={Styles.submit} type="submit">Entrar</button>
-          <Link data-testid='signup' to={'/signup'} className={Styles.link}>Criar conta</Link>
+          <Input type="email" name="email" placeholder="Digite seu e-mail" />
+          <Input
+            type="password"
+            name="password"
+            placeholder="Digite sua senha"
+          />
+          <button
+            data-testid="submit"
+            disabled={!!state.emailError || !!state.passwordError}
+            className={Styles.submit}
+            type="submit"
+          >
+            Entrar
+          </button>
+          <Link data-testid="signup" to={'/signup'} className={Styles.link}>
+            Criar conta
+          </Link>
           <Form />
         </form>
       </Context.Provider>
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
