87cb8469902de6800bdb8bf0faab73de76b03705
Rendrikson98 <rendrikson16@gmail.com>
2022-08-29 12:21:20 -0300

    refactor: change link name

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -163,7 +163,7 @@ describe('Login component', () => {
 
   test('Should go to signup page', async () => {
     const { sut } = makeSut();
-    const register = sut.getByTestId('signup');
+    const register = sut.getByTestId('signup-link');
     fireEvent.click(register);
     expect(history.location.pathname).toBe('/signup');
   });
