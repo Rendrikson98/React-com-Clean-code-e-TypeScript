dabcbcb97be3c07e6357aa846f5471eb1e0a3af6
Rendrikson98 <rendrikson16@gmail.com>
2022-09-05 12:32:07 -0300

    refactor: adapt layout to mobile

--- src/presentation/componentes/login-header/login-header.tsx
+++ src/presentation/componentes/login-header/login-header.tsx
@@ -1,15 +1,14 @@
-import React, { memo } from 'react'
-import Logo from '../logo/logo'
-import Styles from './login-header-styles.scss'
-
+import React, { memo } from 'react';
+import Logo from '../logo/logo';
+import Styles from './login-header-styles.scss';
 
 const LoginHeader = () => {
   return (
-    <header className={Styles.header}>
+    <header className={Styles.headerWrap}>
       <Logo />
       <h1>4Dev - Enquetes para Programadores</h1>
     </header>
-  )
-}
+  );
+};
 
-export default memo(LoginHeader)
+export default memo(LoginHeader);
