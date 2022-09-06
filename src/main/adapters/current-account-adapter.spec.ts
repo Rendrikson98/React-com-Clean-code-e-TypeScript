b04841f6d23651f067a4d126b061d038de957655
Rendrikson98 <rendrikson16@gmail.com>
2022-09-06 14:33:55 -0300

    feat: add getCurrentAccountAdapter

--- src/main/adapters/current-account-adapter.spec.ts
+++ src/main/adapters/current-account-adapter.spec.ts
@@ -1,13 +1,16 @@
 import { UnexpectedError } from '@/domain/erros';
 import { mockAccountModel } from '@/domain/teste';
 import { LocalStorageAdapter } from '@/infra/cache/local-storage-adapter';
-import { setCurrentAccountAdapter } from './current-account-adapter';
+import {
+  getCurrentAccountAdapter,
+  setCurrentAccountAdapter,
+} from './current-account-adapter';
 
 jest.mock('@/infra/cache/local-storage-adapter'); // vai mockar todos os objetos internos e não vai usar o localStorage de fato
 //caso queira ver isso na prática coloque um console logo depois do expect com o localStorage, faço rodar com o mock e sem o mock e vc verá que sem ele salva no localStorage
 
 describe('current-account-adapter', () => {
-  test('Should call LocalStorageAdapter with correct values', () => {
+  test('Should call LocalStorageAdapter.set with correct values', () => {
     const account = mockAccountModel();
     const setSpy = jest.spyOn(LocalStorageAdapter.prototype, 'set');
     setCurrentAccountAdapter(account);
@@ -18,5 +21,15 @@ describe('current-account-adapter', () => {
     expect(() => {
       setCurrentAccountAdapter(undefined);
     }).toThrow(new UnexpectedError()); //testa se a função retorna uma exceção
+  });
+
+  test('Should call LocalStorageAdapter.get with correct values', () => {
+    const account = mockAccountModel();
+    const getSpy = jest
+      .spyOn(LocalStorageAdapter.prototype, 'get')
+      .mockReturnValueOnce(account);
+    const result = getCurrentAccountAdapter();
+    expect(getSpy).toHaveBeenCalledWith('account');
+    expect(result).toEqual(account);
   });
 });
--- src/main/factories/cache/local-storage-adapter-factory.ts
+++ src/main/factories/cache/local-storage-adapter-factory.ts
@@ -1,6 +1,7 @@
+import { GetStorage } from '@/data/Protocols/cache';
 import { SetStorage } from '@/data/Protocols/cache/set-storage';
 import { LocalStorageAdapter } from '@/infra/cache/local-storage-adapter';
 
-export const makeLocalStorageAdapter = (): SetStorage => {
+export const makeLocalStorageAdapter = (): SetStorage & GetStorage => {
   return new LocalStorageAdapter();
 };
