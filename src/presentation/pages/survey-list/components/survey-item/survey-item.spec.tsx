7f16c9c4d7bbd9ec9fe7480ce3c2465b6e0aad38
Rendrikson98 <rendrikson16@gmail.com>
2022-09-14 12:09:04 -0300

    feat: ensure SurveyItem load icon and day correctly

--- src/presentation/pages/survey-list/components/survey-item/survey-item.spec.tsx
+++ src/presentation/pages/survey-list/components/survey-item/survey-item.spec.tsx
@@ -4,16 +4,36 @@ import SurveyItem from './survey-item';
 import { mockSurveyModel } from '@/domain/teste';
 import { IconName } from '@/presentation/componentes';
 
+const makeSut = (survey = mockSurveyModel()): void => {
+  render(<SurveyItem survey={survey} />);
+};
+
 describe('SurveyItem Component', () => {
   test('should render with correct values', () => {
-    const survey = mockSurveyModel();
-    survey.didAnswer = true;
-    survey.date = new Date('2020-01-10T00:00:00');
-    render(<SurveyItem survey={survey} />);
+    const survey = Object.assign(mockSurveyModel(), {
+      didAnswer: true,
+      date: new Date('2020-01-10T00:00:00'),
+    });
+    makeSut(survey);
     expect(screen.getByTestId('icon')).toHaveProperty('src', IconName.thumbUp);
     expect(screen.getByTestId('question')).toHaveTextContent(survey.question);
     expect(screen.getByTestId('day')).toHaveTextContent('10');
     expect(screen.getByTestId('moth')).toHaveTextContent('jan');
     expect(screen.getByTestId('year')).toHaveTextContent('2020');
+  });
+  test('should render with correct values', () => {
+    const survey = Object.assign(mockSurveyModel(), {
+      didAnswer: false,
+      date: new Date('2019-05-03T00:00:00'),
+    });
+    makeSut(survey);
+    expect(screen.getByTestId('icon')).toHaveProperty(
+      'src',
+      IconName.thumbDown
+    );
+    expect(screen.getByTestId('question')).toHaveTextContent(survey.question);
+    expect(screen.getByTestId('day')).toHaveTextContent('03');
+    expect(screen.getByTestId('moth')).toHaveTextContent('mai');
+    expect(screen.getByTestId('year')).toHaveTextContent('2019');
   });
 });
--- src/presentation/pages/survey-list/components/survey-item/survey-item.tsx
+++ src/presentation/pages/survey-list/components/survey-item/survey-item.tsx
@@ -8,13 +8,15 @@ type Props = {
 };
 
 const SurveyItem: React.FC<Props> = ({ survey }: Props) => {
+  const iconName = survey.didAnswer ? IconName.thumbUp : IconName.thumbDown;
   return (
     <li className={Styles.surveyItemWrap}>
       <div className={Styles.surveyContent}>
-        <Icon className={Styles.iconWrap} iconName={IconName.thumbUp} />
+        <Icon className={Styles.iconWrap} iconName={iconName} />
         <time>
           <span data-testid="day" className={Styles.day}>
-            {survey.date.getDate()}
+            {survey.date.getDate().toString().padStart(2, '0')}{' '}
+            {/*o padStart serve para informar que vai ter no máximo 2 caractere e se não tiver adiciona o 0 no inicio*/}
           </span>
           <span data-testid="moth" className={Styles.month}>
             {survey.date
