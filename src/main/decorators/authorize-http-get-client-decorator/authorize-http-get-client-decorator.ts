e25f5f1297c74ebe8fe398c4f416186bcad7782e
Rendrikson98 <rendrikson16@gmail.com>
2022-09-28 16:02:38 -0300

    refactor: ensure RemoteLoadSurveyList map remote data to domain data

--- src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.ts
+++ src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.ts
@@ -12,10 +12,12 @@ export class AuthorizeHttpGetClientDecorator implements HttpGetClient {
   ) {}
   async get(params: HttpGetParams): Promise<HttpResponse> {
     const account = this.getStorage.get('account');
+    //se não existir um account no localStorage ele simplesmente vai pular o if e retornar get da reequisição
     if (account?.accessToken) {
       Object.assign(params, {
         headers: Object.assign(params.headers || {}, {
-          'x-access-token': account.accessToken,
+          //se o params.headers vier vazio ou undefined ele muda pra um objeto vazio {}
+          'x-access-token': account.accessToken, //o 'x-access-token' é o nome de envio do token do back
         }),
       });
     }
