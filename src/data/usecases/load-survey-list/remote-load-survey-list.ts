802b27f8f86fc314c2b8023c5f6963cc26c86bf1
Rendrikson98 <rendrikson16@gmail.com>
2022-09-28 15:54:52 -0300

    refactor: ensure RemoteLoadSurveyList map remote data to domain data

--- src/data/usecases/load-survey-list/remote-load-survey-list.ts
+++ src/data/usecases/load-survey-list/remote-load-survey-list.ts
@@ -10,10 +10,12 @@ export class RemoteLoadSurveyList {
 
   async loadAll(): Promise<LoadSuveyList.Model[]> {
     const httpResponse = await this.httpGetClient.get({ url: this.url });
-
+    const remoteSurveys = httpResponse.body || []; //se httpResponse.body for vazio ou undefined troque por um array vazio []
     switch (httpResponse.statusCode) {
       case HttpStatusCode.ok:
-        return httpResponse.body;
+        return remoteSurveys.map((remoteSurvey) =>
+          Object.assign(remoteSurvey, { date: new Date(remoteSurvey.date) })
+        );
       case HttpStatusCode.noContent:
         return [];
       default:
@@ -24,5 +26,10 @@ export class RemoteLoadSurveyList {
 //o nome dessa padrão é TypeAlias onde criamos um namespace com o mesmo nome da interface e usamos os tipos por meio de chamadas no namespace.
 //Nesse caso estamos criando um namespace para a classe, já que já fizemos a da interface na camada de domain
 export namespace RemoteLoadSurveyList {
-  export type Model = LoadSuveyList.Model;
+  export type Model = {
+    id: string;
+    question: string;
+    date: string;
+    didAnswer: boolean;
+  };
 }
