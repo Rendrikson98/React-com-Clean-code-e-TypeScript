78b1158bc9df813cab4c4a0301f1e580919c8bce
Rendrikson98 <rendrikson16@gmail.com>
2022-09-28 15:44:02 -0300

    feat: add makeAuthorizeHttpGetClientDecorator factory

--- src/main/factories/usecases/load-survey-list/remote-load-seuvey-list.ts
+++ src/main/factories/usecases/load-survey-list/remote-load-seuvey-list.ts
@@ -1,8 +1,11 @@
 import { RemoteLoadSurveyList } from '@/data/usecases/load-survey-list/remote-load-survey-list';
 import { LoadSuveyList } from '@/domain/usecases/load-suvery-list';
-import { makeAxiosHttpClient } from '@/main/factories/http/axios-http-client-factory';
+import { makeAuthorizeHttpGetClientDecorator } from '../../decorators';
 import { MakeApiURL } from '../../http/api-url-factory';
 
 export const MakeRemoteSurveyList = (): LoadSuveyList => {
-  return new RemoteLoadSurveyList(MakeApiURL('surveys'), makeAxiosHttpClient());
+  return new RemoteLoadSurveyList(
+    MakeApiURL('surveys'),
+    makeAuthorizeHttpGetClientDecorator()
+  );
 };
