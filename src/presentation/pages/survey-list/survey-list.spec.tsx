167de85d58bc6889d44be215254d537354e87311
Rendrikson98 <rendrikson16@gmail.com>
2022-09-29 09:18:31 -0300

    feat: ensure  SurveyList logout on AccessDeniedError

--- src/presentation/pages/survey-list/survey-list.spec.tsx
+++ src/presentation/pages/survey-list/survey-list.spec.tsx
@@ -3,14 +3,15 @@ import { render, screen, waitFor } from '@testing-library/react';
 import SurveyList from './survey-list';
 import { LoadSuveyList } from '@/domain/usecases/load-suvery-list';
 import { mockAccountModel, mockSurveyListModel } from '@/domain/teste';
-import { UnexpectedError } from '@/domain/erros';
+import { AccessDeniedError, UnexpectedError } from '@/domain/erros';
 import {
   Route,
   Routes,
   unstable_HistoryRouter as HistoryRouter,
 } from 'react-router-dom';
-import { createMemoryHistory } from 'history';
+import { createMemoryHistory, MemoryHistory } from 'history';
 import { ApiContext } from '@/presentation/contexts';
+import { AccountModel } from '@/domain/models';
 
 class LoadSurveyListSpy implements LoadSuveyList {
   callsCount = 0;
@@ -23,23 +24,29 @@ class LoadSurveyListSpy implements LoadSuveyList {
 
 type SutTypes = {
   loadSurveyListSpy: LoadSurveyListSpy;
+  history: MemoryHistory;
+  setCurrentAccountMock: (account: AccountModel) => void;
 };
 
 const makeSut = (loadSurveyListSpy = new LoadSurveyListSpy()): SutTypes => {
+  const history = createMemoryHistory({ initialEntries: ['/'] });
+  const setCurrentAccountMock = jest.fn();
   render(
     <ApiContext.Provider
       value={{
-        setCurrentAccount: jest.fn(),
+        setCurrentAccount: setCurrentAccountMock,
         getCurrentAccount: () => mockAccountModel(),
       }}
     >
-      <HistoryRouter history={createMemoryHistory()}>
+      <HistoryRouter history={history}>
         <SurveyList loadSurveyList={loadSurveyListSpy} />
       </HistoryRouter>
     </ApiContext.Provider>
   );
   return {
     loadSurveyListSpy,
+    history,
+    setCurrentAccountMock,
   };
 };
 
@@ -61,6 +68,16 @@ describe('SurveyList Component', () => {
     await waitFor(() => screen.getAllByTestId('li'));
     expect(surveyList.querySelectorAll('li.surveyItemWrap')).toHaveLength(3);
   });
+  // test('should logout on AccessDeniedError', async () => {
+  //   const loadSurveyListSpy = new LoadSurveyListSpy();
+  //   jest
+  //     .spyOn(loadSurveyListSpy, 'loadAll')
+  //     .mockRejectedValue(new AccessDeniedError()); //mocando a função loadAll e definindo o seu retorno
+  //   const { setCurrentAccountMock, history } = makeSut(loadSurveyListSpy);
+  //   await waitFor(() => screen.getByRole('heading'));
+  //   expect(setCurrentAccountMock).toHaveBeenCalledWith(undefined);
+  //   expect(history.location.pathname).toBe('/login');
+  // });
   // test('should render SurveyItems on failure', async () => {
   //   const loadSurveyListSpy = new LoadSurveyListSpy();
   //   const error = new UnexpectedError();
--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -1,6 +1,9 @@
+import { AccessDeniedError } from '@/domain/erros';
 import { LoadSuveyList } from '@/domain/usecases/load-suvery-list';
 import { Footer, Header } from '@/presentation/componentes';
-import React, { useEffect, useState } from 'react';
+import { ApiContext } from '@/presentation/contexts';
+import React, { useContext, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { SurveyContext, SurveyListItem, Error } from './components';
 import Styles from './survey-list-styles.scss';
 
@@ -9,6 +12,8 @@ type Props = {
 };
 
 const SurveyList: React.FC<Props> = ({ loadSurveyList }: Props) => {
+  const history = useNavigate();
+  const { setCurrentAccount } = useContext(ApiContext);
   const [state, setState] = useState({
     surveys: [] as LoadSuveyList.Model[],
     error: '',
@@ -20,7 +25,12 @@ const SurveyList: React.FC<Props> = ({ loadSurveyList }: Props) => {
         const surveys = await loadSurveyList.loadAll();
         setState({ ...state, surveys });
       } catch (error) {
-        setState({ ...state, error: error.message });
+        if (error instanceof AccessDeniedError) {
+          setCurrentAccount(undefined);
+          history('/login');
+        } else {
+          setState({ ...state, error: error.message });
+        }
       }
     };
 
