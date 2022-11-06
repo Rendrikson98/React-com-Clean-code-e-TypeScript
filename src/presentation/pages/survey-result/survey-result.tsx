05b9be086a437160cbeec6a3315598584662b414
Rendrikson98 <rendrikson16@gmail.com>
2022-11-06 19:40:14 -0300

    feat: ensure suveyResult reload on button click

--- src/presentation/pages/survey-result/survey-result.tsx
+++ src/presentation/pages/survey-result/survey-result.tsx
@@ -6,24 +6,38 @@ import Loading from '@/presentation/componentes/loading/loading';
 import { LoadSuveyResult } from '@/domain/usecases';
 import { Error } from '../survey-list/components';
 import { LoadSurveyResultSpy } from '@/domain/teste';
+import { useErrorHandler } from '@/presentation/hooks';
 
 type Props = {
   loadSurveyResult: LoadSurveyResultSpy;
 };
 
 const SurveyResult: React.FC<Props> = ({ loadSurveyResult }: Props) => {
+  const handleError = useErrorHandler((error: Error) => {
+    setState({ ...state, surveyResult: null, error: error.message });
+  });
   const [state, setState] = useState({
     isLoading: false,
     error: '',
     surveyResult: null as LoadSuveyResult.Model,
+    reload: false,
   });
+
+  const reload = (): void => {
+    setState((old) => ({
+      isLoading: false,
+      surveyResult: null,
+      error: '',
+      reload: !old.reload,
+    }));
+  };
 
   useEffect(() => {
     loadSurveyResult
       .load()
       .then((surveyResult) => setState((old) => ({ ...old, surveyResult })))
-      .catch();
-  }, []);
+      .catch(handleError);
+  }, [state.reload]);
 
   return (
     <div className={Styles.surveyResultWrap}>
@@ -66,7 +80,7 @@ const SurveyResult: React.FC<Props> = ({ loadSurveyResult }: Props) => {
           </>
         )}
         {state.isLoading && <Loading />}
-        {state.error && <Error error={state.error} reload={() => {}} />}
+        {state.error && <Error error={state.error} reload={reload} />}
       </div>
       <Footer />
     </div>
