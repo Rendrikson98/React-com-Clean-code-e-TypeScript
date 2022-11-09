98e8f72ede172aadded8af7e39f2e6e9b97bb3ac
Rendrikson98 <rendrikson16@gmail.com>
2022-11-09 11:09:41 -0300

    feat: ensure SurveyResult presents new result on answer click

--- src/presentation/pages/survey-result/survey-result.tsx
+++ src/presentation/pages/survey-result/survey-result.tsx
@@ -36,7 +36,9 @@ const SurveyResult: React.FC<Props> = ({
     setState((old) => ({ ...old, isLoading: true }));
     saveSurveyResult
       .save({ answer })
-      .then((surveyResult) => setState((old) => ({ ...old, surveyResult })))
+      .then((surveyResult) =>
+        setState((old) => ({ ...old, isLoading: false, surveyResult }))
+      )
       .catch(handleError);
   };
 
